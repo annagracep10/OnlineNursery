e6b279dc7c78adc4c002097ad958b566d0d2f538
annagracep10 <anna.edwin@people10.com>
2024-04-23 11:52:22 +0530

    Logging working in console

--- src/main/java/com/techphantomexample/usermicroservice/services/UserServicesImp.java
+++ src/main/java/com/techphantomexample/usermicroservice/services/UserServicesImp.java
@@ -2,17 +2,21 @@ package com.techphantomexample.usermicroservice.services;
 import com.techphantomexample.usermicroservice.model.User;
 import com.techphantomexample.usermicroservice.repository.UserRepository;
 import io.micrometer.common.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserServicesImp implements UserService
 {
+    private static final Logger log = LoggerFactory.getLogger(UserServicesImp.class);
     @Autowired
     UserRepository userRepository;
 
@@ -29,18 +33,22 @@ public class UserServicesImp implements UserService
         String userRole = user.getUserRole();
 
         if (StringUtils.isBlank(userFullName) ||StringUtils.isBlank(userEmail) || StringUtils.isBlank(userPassword) || StringUtils.isBlank(userRole)) {
+            log.error("Missing Fields");
             return "All fields are required";
         }
 
         if (!isValidEmail(userEmail)) {
+            log.error("Invalid email address");
             return "Invalid email address";
         }
 
         if (existsByEmail(userEmail)) {
+            log.error("User already exists");
             return "User with provided Email ID exists";
         }
 
         if (!isValidPassword(userPassword)) {
+            log.error("Password format wrong");
             return "Password must be at least 8 characters long, contain at least one uppercase letter, and at least one digit";
         }
 
@@ -48,10 +56,12 @@ public class UserServicesImp implements UserService
         user.setUserPassword(hashedPassword);
 
         if (!isValidUserRole(userRole)) {
+            log.error("Entered userRole is incorrect");
             return "User role should be one among: ADMIN, SUPERVISOR, BUYER, SELLER";
         }
 
         userRepository.save(user);
+        log.info("User Created successfully");
         return "User Created successfully";
     }
 
@@ -65,12 +75,14 @@ public class UserServicesImp implements UserService
 
             String newEmail = newUserDetails.getUserEmail();
             if (newEmail != null && !isValidEmail(newEmail)) {
+                log.error("Invalid email address updated");
                 return "Invalid email address";
             }
             existingUser.setUserEmail(newEmail);
 
             String newPassword = newUserDetails.getUserPassword();
             if (newPassword != null && !isValidPassword(newPassword)) {
+                log.error(" Updated Password format wrong");
                 return "Password must be at least 8 characters long, contain at least one uppercase letter, and at least one digit";
             }
             String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
@@ -78,13 +90,14 @@ public class UserServicesImp implements UserService
 
             String newUserRole = newUserDetails.getUserRole();
             if (newUserRole != null && !isValidUserRole(newUserRole)) {
+                log.error("Updated userRole is incorrect");
                 return "User role should be one among: ADMIN, SUPERVISOR, BUYER, SELLER";
             }
             existingUser.setUserRole(newUserRole);
 
 
             userRepository.save(existingUser);
-
+            log.info("User Updated successfully");
             return "User Updated Successfully";
         } else {
 
@@ -97,8 +110,10 @@ public class UserServicesImp implements UserService
         if (userRepository.existsById(userId))
         {
             userRepository.deleteById(userId);
+            log.info("User deleted");
             return "User Deleted Successfully";
         }
+        log.info("User does not exists");
         return "User Not Found";
     }
 
@@ -106,15 +121,16 @@ public class UserServicesImp implements UserService
     public User getUser(int userId) {
         if (userRepository.existsById(userId))
         {
+            log.info("User found and returned");
             return userRepository.findById(userId).get();
         }
+        log.info("user not found");
         return null;
     }
 
     @Override
     public List<User> getAllUsers() {
-
-        userRepository.findAll();
+        log.info("Returning all users in database");
         return userRepository.findAll();
     }
 

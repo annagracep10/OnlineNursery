837d5ad09b43d4cfc7b4fbbf1ca75f81fc01ec01
annagracep10 <anna.edwin@people10.com>
2024-07-04 15:27:28 +0530

    cart web controller changed to mockmvc

--- src/test/java/com/techphantomexample/usermicroservice/apiController/UserApiControllerTest.java
+++ src/test/java/com/techphantomexample/usermicroservice/apiController/UserApiControllerTest.java
@@ -9,7 +9,6 @@ import com.techphantomexample.usermicroservice.model.Login;
 import com.techphantomexample.usermicroservice.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/com/techphantomexample/usermicroservice/webController/CartWebControllerTest.java
+++ src/test/java/com/techphantomexample/usermicroservice/webController/CartWebControllerTest.java
@@ -1,6 +1,6 @@
 package com.techphantomexample.usermicroservice.webController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techphantomexample.usermicroservice.entity.Cart;
 import com.techphantomexample.usermicroservice.entity.CartItem;
 import com.techphantomexample.usermicroservice.entity.User;
@@ -10,87 +10,92 @@ import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(CartWebController.class)
 @ExtendWith(MockitoExtension.class)
 public class CartWebControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private CartService cartService;
 
-    @Mock
+    @MockBean
     private UserService userService;
 
-    @Mock
-    private HttpSession session;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    @Mock
-    private Model model;
-
-    @InjectMocks
-    private CartWebController cartWebController;
+    private MockHttpSession session;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        session = new MockHttpSession();
+        User user = new User();
+        user.setUserId(1);
+        session.setAttribute("user", user);
     }
 
     @Test
-    public void testViewCart() {
-        User user = new User();
-        user.setUserId(1);
+    public void testViewCart() throws Exception {
         Cart cart = new Cart();
-        when(session.getAttribute("user")).thenReturn(user);
         when(userService.getCartByUserId(1)).thenReturn(cart);
 
-        String viewName = cartWebController.viewCart(model, session);
-
-        assertEquals("cart", viewName);
-        verify(model, times(1)).addAttribute("cart", cart);
-        verify(model, times(1)).addAttribute("user", user);
+        mockMvc.perform(get("/user/cart")
+                        .session(session))
+                .andExpect(status().isOk())
+                .andExpect(view().name("cart"))
+                .andExpect(model().attribute("cart", cart))
+                .andExpect(model().attributeExists("user"));
     }
 
     @Test
-    public void testAddToCart() {
-        User user = new User();
-        user.setUserEmail("test@example.com");
+    public void testAddToCart() throws Exception {
         CartItem cartItem = new CartItem();
-        when(session.getAttribute("user")).thenReturn(user);
+        cartItem.setId(1);
 
-        String viewName = cartWebController.addToCart(cartItem, session);
-
-        assertEquals("redirect:/user/products", viewName);
+        mockMvc.perform(post("/user/addToCart")
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("cartItemId", String.valueOf(cartItem.getId())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/products"));
     }
 
     @Test
-    public void testRemoveFromCart() {
-        User user = new User();
-        user.setUserId(1);
-        when(session.getAttribute("user")).thenReturn(user);
+    public void testRemoveFromCart() throws Exception {
+        mockMvc.perform(post("/user/removeFromCart")
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("itemId", "1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/cart"));
 
-        String viewName = cartWebController.removeFromCart(1, session);
-
-        assertEquals("redirect:/user/cart", viewName);
-        verify(cartService, times(1)).removeItemFromCart(1, 1);
     }
 
     @Test
-    public void testCheckout() throws JsonProcessingException {
-        User user = new User();
-        user.setUserId(1);
-        when(session.getAttribute("user")).thenReturn(user);
+    public void testCheckout() throws Exception {
+        mockMvc.perform(post("/user/checkout")
+                        .session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/cart"));
 
-        String viewName = cartWebController.checkout(session);
 
-        assertEquals("redirect:/user/cart", viewName);
-        verify(cartService, times(1)).checkout(1);
     }
 }

dc5d3f473621dee560c4e44f0a43bd87aded3036
annagracep10 <anna.edwin@people10.com>
2024-07-12 17:37:07 +0530

    new branch develop

--- src/main/java/com/techphantomexample/usermicroservice/validator/UserValidator.java
+++ src/main/java/com/techphantomexample/usermicroservice/validator/UserValidator.java
@@ -24,7 +24,6 @@ public class UserValidator {
     }
 
     private static boolean isValidEmail(String email) {
-        // Regular expression for basic email validation
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         return email.matches(emailRegex);
     }

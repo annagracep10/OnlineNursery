a26a4aa406d1b649daf5215e2dfa95aa76a3ffed
annagracep10 <anna.edwin@people10.com>
2024-08-08 16:56:28 +0530

    Uploading Images

--- src/main/java/com/techphantomexample/usermicroservice/dto/PlantRequest.java
+++ src/main/java/com/techphantomexample/usermicroservice/dto/PlantRequest.java
@@ -5,16 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class BaseProductDTO {
+@NoArgsConstructor
+public class PlantRequest {
 
-    private int id;
     private String name;
     private String description;
     private double price;
     private String category;
     private int quantity;
-    private byte[] image;
-
+    private String typeOfPlant;
+    private String sunlightRequirements;
+    private String wateringFrequency;
 }
